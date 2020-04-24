71a1134e73c4b4a61369ade0e90cdc7fc1853c42
Renata Marques <renata.nrmarques@gmail.com>
2020-04-24 14:23:53 -0300

    using hooks for show venue name instead of venue id

--- src/components/VenueListComponent.js
+++ src/components/VenueListComponent.js
@@ -1,15 +1,16 @@
-import React from "react";
+import React, { useState } from "react";
 
 const VenueListComponent = (props) => {
+  const [venue, setVenue] = useState("")
   return (
     <React.Fragment>
       <button onClick={() => { props.handleChange() }}>Load Venues</button>
-        <p>Current Venue: {props.venueId}</p>
+        <p>Current Venue: {venue}</p>
         <p>Click on a venue to select:</p>
         <ul>
           {
             props.venues.map((venue) => (
-              <li key={venue.id} onClick={() => { props.handleVenueCreation(venue.id) }}>
+              <li key={venue.id} onClick={() => { props.handleVenueCreation(venue.id); setVenue(venue.name) }}>
                 {venue.id} - {venue.name}
               </li>
             ))
