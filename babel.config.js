ff415a08c3237127f32780c2d408a4a291853138
Renata Marques <renata.nrmarques@gmail.com>
2020-04-25 21:03:26 -0300

    test configuration

---
+++ babel.config.js
@@ -0,0 +1,20 @@
+module.exports = {
+  presets: [
+    [
+      '@babel/preset-env',
+      {
+        targets: {
+          esmodules: true,
+        },
+      },
+    ],
+    "@babel/preset-react"],
+  plugins: [
+    [
+      "babel-plugin-root-import",
+      {
+        rootPathSuffix: "src"
+      }
+    ]
+  ]
+};
